464c3446ee458311e04d9bfdd64e13779964edb5
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-02-25 20:51:06 -0500

    Update Anho_nacimiento.js

--- Anho_nacimiento.js
+++ Anho_nacimiento.js
@@ -1,5 +1,5 @@
 alert("hola, voy a calcular tu año de nacimiento, dependiendo tu edad");
 let edad = prompt("¿Que edad tienes?");
 let anho_actual = 2021;
-let anho_nacimiento = anho_actual + edad;
+let anho_nacimiento = anho_actual - edad;
 alert("Tu tienes " + anho_nacimiento + " años"); 
