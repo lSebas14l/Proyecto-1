4b16f71db422608571db37615981d1664117a9f7
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-03 21:45:55 -0500

    progress of the exercises 1

---
+++ taller 1/ejercicio1.js
@@ -0,0 +1,9 @@
+//Comenzamos pidiendo los datos al usuario
+alert("Bienvenido en este ejercicio vamos a sumar y restar 2 numeros que usted elija");
+var dato_1 = parseInt (prompt("Ingrese un numero"));
+var dato_2 = parseint (prompt("Ingrese otro numero"));
+
+let suma = dato_1 + dato_2;
+let resta = dato_1 - dato_2;
+
+alert("La suma de los numeros dados es " + suma);
