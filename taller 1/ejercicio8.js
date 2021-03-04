8f4488a4ca998ce7024b85d949c85eb003c7ae92
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-03 23:01:52 -0500

    Exercise 8

---
+++ taller 1/ejercicio8.js
@@ -0,0 +1,13 @@
+// Pedimos la información necesaria para la actividad
+alert("En esta actividad vamos a ayudar a Juan a saber a cuantas personas puede invitar al estadio");
+alert("La entrada al estadio cuesta $8.000 por persona");
+
+let valor_boleta = 8000;
+let dinero_juan = parseInt(prompt("¿Cual es tu presupuesto Juan?"));
+
+if (dinero_juan >= valor_boleta) {
+    var amigos_invitados = Math.floor(dinero_juan/valor_boleta);
+}
+
+
+alert("Puedes invitar a: " + amigos_invitados+ " Amigos");
