fdb203fd789e4039a41899bc607de92e0c5e4e4e
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-03 22:07:32 -0500

    Exercise 4

---
+++ taller 1/ejercicio4.js
@@ -0,0 +1,11 @@
+//Comenzamos brindando la información suministrada
+alert("En este ejercicio diremos cual es la edad de Pepito, José y Carlos");
+alert("Pepito tiene: 18,  José tiene: 20,  Carlos tiene: 21")
+
+let pepito = 18;
+let jose = 20;
+let carlos = 21;
+
+let suma = pepito + jose + carlos;
+
+alert("La edad todal de estas tres personas es: "+ suma);
