ae385d27eaf47472f2d24ead90167d01175f7fee
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-03 22:27:06 -0500

    Execises 6 & 7

--- taller 1/ejercicio6.js
+++ taller 1/ejercicio6.js
@@ -1,9 +1,9 @@
 //Brindamos la info suministrada
 alert("En esta actividad vamos a hallar el área de un rectangulo que mide 5 metros de largo por 8 de ancho");
-
+//Guardamos la información
 let largo_rectangulo = 5;
 let ancho_rectangulo = 8;
-
+//Hacemos las operaciones matematicas
 let area_rectangulo = largo_rectangulo*ancho_rectangulo;
-
+//Entregamos el resultado
 alert("El área del rectangulo es: "+ area_rectangulo);
---
+++ taller 1/ejercicio7.js
@@ -0,0 +1,10 @@
+//Brindamos la info suministrada
+alert("En esta actvidad vamos a mostrar en pantalla cuanto dinero le hace falta a María para entrar al cine");
+alert("María solo tiene $6.700 y la entrada al cine cuesta $8.500");
+
+let dinero_maria = 6700;
+let valor_cine = 8500;
+
+let dinero_faltante = valor_cine-dinero_maria;
+
+alert("A María le hacen falta: $" + dinero_faltante);
