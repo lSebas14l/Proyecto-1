bcebd68ae3e207baf1ea4171d47422212c553afe
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-03 21:57:24 -0500

    Exercise2

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+//Comenzamos pidiendo los datos
+
+alert("Vamos a sacar la raiz cuadrada y la cubica de un número de su elección");
+let dato = parseInt(prompt("Ingrese un valor para sacar la raiz cubica y cuadrada"));
+
+//Realizamos las operaciones matematicas
+let raiz_cuadrada = dato*dato;
+
+let raiz_cubica = dato*dato*dato;
+
+//Se entrega el resultado
+
+alert("La raíz cuadrada es: " + raiz_cuadrada);
+alert("La raíz cubica es: " + raiz_cubica);
--- taller 1/ejercicio1.js
+++ taller 1/ejercicio1.js
@@ -1,9 +1,14 @@
 //Comenzamos pidiendo los datos al usuario
 alert("Bienvenido en este ejercicio vamos a sumar y restar 2 numeros que usted elija");
-var dato_1 = parseInt (prompt("Ingrese un numero"));
-var dato_2 = parseint (prompt("Ingrese otro numero"));
 
+let dato_1 = parseInt(prompt("Ingrese un numero"));
+
+let dato_2 = parseInt(prompt("Ingrese otro numero"));
+
+//Realizamos las operaciones
 let suma = dato_1 + dato_2;
 let resta = dato_1 - dato_2;
 
+//Se entregan los resultados
 alert("La suma de los numeros dados es " + suma);
+alert("La resta de los numeros dados es " + resta);
