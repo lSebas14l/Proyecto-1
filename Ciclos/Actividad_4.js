a8cd979304120354c8519d73179a201411eb7b00
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-07 16:32:01 -0500

    Exercise 4

---
+++ Ciclos/Actividad_4.js
@@ -0,0 +1,12 @@
+//Leer dos números y mostrar todos los enteros comprendidos entre ellos
+
+let numero1 = parseInt (prompt("ingrese el primer numero para calcular los enteros"));
+
+let numero2 = parseInt (prompt("ingrese el segundo numero para calcular los enteros"));
+for (let i = 1; i <= numero1; i++) {
+   console.log ("enteros numero 1: " + i);
+ for (let x = 1; x <= numero2; x++) {
+   console.log ("enteros numero 1: " + x);
+    
+ }   
+}
