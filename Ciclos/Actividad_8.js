9bcd503e11589bd466196af6e1fd38c8d5f0d3a2
Sebas.Gaviria <sgaviria@unitecnica.net>
2021-03-07 18:41:45 -0500

    Advance

--- Ciclos/Actividad_8.js
+++ Ciclos/Actividad_8.js
@@ -1,7 +1,8 @@
 alert("En esta actividad vamos a mostrar los enteros del 1 al 100");
-let valor = 100;
+let valor = 200;
+let i = parseInt(20);
+for (i ; i <= valor; i = i+2) {
 
-for (let i = 1; i < valor; i++) {
     console.log(i)
     
 }
